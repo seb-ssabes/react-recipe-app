78ab92a67746ba13c33d529ef56c952520c9463d
Sebastian Olivares <olivares.heredia@gmail.com>
2025-09-09 12:07:24 +0200

    recipeItem created and passed to home page results

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -3,21 +3,35 @@ import { GlobalContext } from "./GlobalContext";
 
 export default function GlobalState({children}) {
   const [searchParam, setSearchParam] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [recipeList, setRecipeList] = useState([])
 
   async function handleSubmit(event) {
     event.preventDefault()
+
 
     try {
       const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`)
       const data = await response.json()
       console.log(data)
+
+      if(data.data?.recipes) {
+        setRecipeList(data?.data?.recipes)
+        setLoading(false)
+        setSearchParam('')
+      }
+
     }catch(error){
       console.log(error)
+      setLoading(false)
+      setSearchParam('')
     }
   }
 
+  console.log(loading, recipeList)
+
   return (
-    <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit }}>
+    <GlobalContext.Provider value={{ searchParam, loading, recipeList, setSearchParam, handleSubmit }}>
       {children}
     </GlobalContext.Provider>
   )
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,25 @@
+import { useContext } from "react"
+import { GlobalContext } from "../../context/GlobalContext"
+import RecipeItem from "../../components/recipe-item"
+
+
 export default function Home() {
+  const {recipeList, loading} = useContext(GlobalContext)
+
+  if (loading) return <div>Loading...Please wait.</div>
 
   return (
-    <div className='bg-gray-500'>Home</div>
+    <div className='py8 container mx-auto flex flex-wrap justify-center gap-10'>
+      {
+        recipeList && recipeList.length > 0
+        ?
+          recipeList.map((item) => <RecipeItem item={item}/>)
+        :
+          <div>
+            <p className="lg:text-4xl text-xl text-center text-black font-bold">No recipes found. Please try a different ingredient</p>
+          </div>
+      }
+    </div>
+
   )
 }
